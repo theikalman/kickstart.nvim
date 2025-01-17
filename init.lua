1cec06ea745bb5b8358ff1352a772e086a305774
AjiYakin <ajiyakin91@gmail.com>
2025-01-17 22:17:31 +0700

    reword for keymap of open diagnostic quick fix

--- init.lua
+++ init.lua
@@ -81,7 +81,7 @@ vim.wo.wrap = false
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open Diag. Quickfix List' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
