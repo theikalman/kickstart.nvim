199f04cdacbf7f9ef97956b7af780050d992d77e
AjiYakin <ajiyakin91@gmail.com>
2025-01-18 23:32:41 +0700

    add php debugger to masson

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -102,6 +102,7 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
         'debugpy',
+        'php-debug-adapter',
       },
     }
 
