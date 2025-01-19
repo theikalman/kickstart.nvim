04ffc69e1dc03cbe85fce31a84a12ea92b59fd6e
AjiYakin <ajiyakin91@gmail.com>
2025-01-19 19:32:10 +0700

    import treesitter plugin instead

--- init.lua
+++ init.lua
@@ -909,61 +909,6 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'go',
-        'gomod',
-        'gosum',
-        'gotmpl',
-        'gowork',
-        'html',
-        'javascript',
-        'jsdoc',
-        'json',
-        'jsonc',
-        'lua',
-        'luadoc',
-        'luap',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'proto',
-        'python',
-        'query',
-        'sql',
-        'toml',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
   {
     -- A Neovim plugin for setting the commentstring option based on the cursor location in the file.
     -- The location is checked via treesitter queries. THIS PLUGIN REQUIRE TREESITTER ABOVE.
@@ -981,6 +926,7 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.lazygit-nvim',
   require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.nvim-treesitter',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
