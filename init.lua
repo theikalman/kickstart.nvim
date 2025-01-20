f34beee3afd22c16fb52814ce33aefbcbd87e1a8
AjiYakin <ajiyakin91@gmail.com>
2025-01-20 16:56:39 +0700

    add flash plugin for fast search

--- init.lua
+++ init.lua
@@ -934,6 +934,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.vim-dadbod-ui',
   require 'kickstart.plugins.persistence-nvim',
+  require 'kickstart.plugins.flash-nvim',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
