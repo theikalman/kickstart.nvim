7d63e8897cf74431c4fef8ec65d01d36429005fc
AjiYakin <ajiyakin91@gmail.com>
2025-01-26 16:30:41 +0700

    not working

--- lua/kickstart/plugins/nvim-treesitter.lua
+++ lua/kickstart/plugins/nvim-treesitter.lua
@@ -45,15 +45,6 @@ return { -- Highlight, edit, and navigate code
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      select = {
-        enable = true,
-        keymaps = {
-          ['am'] = '@function.outer',
-          ['im'] = '@function.inner',
-        },
-      },
-    },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
