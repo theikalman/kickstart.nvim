bb9ae9401cefed95f2873f7cbc7476cc6d543311
AjiYakin <ajiyakin91@gmail.com>
2025-02-03 09:30:00 +0700

    add fold method for supported languages with treesitter

--- init.lua
+++ init.lua
@@ -203,6 +203,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Add folding method for supported language
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+
+    -- check if treesitter has parser 
+    if require("nvim-treesitter.parsers").has_parser() then
+
+      -- use treesitter folding
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+
+      -- use alternative foldmethod
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
+-- disable folding on startup
+vim.opt.foldenable = false
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
