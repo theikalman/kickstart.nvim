343767505ef4e0984fd46a0afb3a4fd52ff0ddd4
AjiYakin <ajiyakin91@gmail.com>
2025-02-04 13:53:21 +0700

    ignore node_modules and vendor from telescope

--- init.lua
+++ init.lua
@@ -389,11 +389,15 @@ require("lazy").setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "vendor",
+          },
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+        },
         -- pickers = {}
         extensions = {
           ["ui-select"] = {
