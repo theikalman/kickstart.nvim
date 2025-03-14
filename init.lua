c7799ee8113c033983fd08409294bf1338e3f9b2
AjiYakin <ajiyakin91@gmail.com>
2025-03-14 19:00:34 +0700

    use rose theme

--- init.lua
+++ init.lua
@@ -869,44 +869,6 @@ require("lazy").setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    "folke/tokyonight.nvim",
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    -- vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    --   vim.cmd.hi 'Comment gui=none'
-    -- end,
-  },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    init = function()
-      -- valid values are: catppuccin-latte catppuccin-frappe catppuccin-macchiato catppuccin-mocha
-      vim.cmd.colorscheme("catppuccin-macchiato")
-      vim.cmd.hi("Comment gui=none")
-    end,
-    config = function()
-      local catpuccin = require("catppuccin")
-      catpuccin.setup({
-        background = {
-          light = "latte",
-          dark = "macchiato",
-        },
-      })
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   {
     "folke/todo-comments.nvim",
@@ -980,6 +942,7 @@ require("lazy").setup({
   require("kickstart.plugins.flash-nvim"),
   require("kickstart.plugins.rest-nvim"),
   require("kickstart.plugins.vim-illuminate"),
+  require("kickstart.plugins.theme"),
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
