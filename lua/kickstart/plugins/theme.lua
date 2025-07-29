12bccb26997522a1ea0805d031ecaaab61549778
AjiYakin <ajiyakin91@gmail.com>
2025-07-29 11:49:46 +0700

    set dim for inactive windows in neovim

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -41,6 +41,11 @@ return {
         dark = "macchiato",
       },
       transparent_background = true,
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+      },
     })
   end,
 }
