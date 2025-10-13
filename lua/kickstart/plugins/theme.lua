9df1a4c5137bab00fc6dc663bc8b299a3a6e58ec
AjiYakin <ajiyakin91@gmail.com>
2025-10-13 15:38:52 +0700

    add e-ink theme!

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -1,3 +1,20 @@
+return {
+   "e-ink-colorscheme/e-ink.nvim",
+   priority = 1000,
+   config = function ()
+      require("e-ink").setup()
+      vim.cmd.colorscheme "e-ink"
+
+      -- choose light mode or dark mode
+      -- vim.opt.background = "dark"
+      -- vim.opt.background = "light"
+      --
+      -- or do
+      -- :set background=dark
+      -- :set background=light
+   end
+}
+
 -- return {
 --   "rose-pine/neovim",
 --   name = "rose-pine",
@@ -6,23 +23,23 @@
 --   end
 -- }
 
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  "folke/tokyonight.nvim",
-  -- priority = 1000, -- Make sure to load this before all the other start plugins.
-  -- init = function()
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  -- vim.cmd.colorscheme 'tokyonight-night'
-
-  -- You can configure highlights by doing something like:
-  --   vim.cmd.hi 'Comment gui=none'
-  -- end,
-}
+-- return { -- You can easily change to a different colorscheme.
+--   -- Change the name of the colorscheme plugin below, and then
+--   -- change the command in the config to whatever the name of that colorscheme is.
+--   --
+--   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--   "folke/tokyonight.nvim",
+--   -- priority = 1000, -- Make sure to load this before all the other start plugins.
+--   -- init = function()
+--   -- Load the colorscheme here.
+--   -- Like many other themes, this one has different styles, and you could load
+--   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--   -- vim.cmd.colorscheme 'tokyonight-night'
+--
+--   -- You can configure highlights by doing something like:
+--   --   vim.cmd.hi 'Comment gui=none'
+--   -- end,
+-- }
 
 -- return {
 --   "catppuccin/nvim",
