765ea98dafe41144d282970df185a9f6ca49c7b6
AjiYakin <aji@AjiYakins-Mac-mini.local>
2025-12-30 11:28:03 +0400

    update

--- init.lua
+++ init.lua
@@ -758,6 +758,7 @@ require("lazy").setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         json = { 'jq' },
+        gopls = { 'gofmt' },
       },
     },
   },
